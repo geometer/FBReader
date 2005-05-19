71246003c599e8aa31274a039f75bab8853e842d
Nikolay Pultsin <geometer@fbreader.org>
2005-05-19 02:54:56 +0000

    my own xml parser: attributes processing added
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@221 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- zlibrary/abstract/xml/own/ZLXMLReaderInternal.cpp
+++ zlibrary/abstract/xml/own/ZLXMLReaderInternal.cpp
@@ -13,7 +13,7 @@ void ZLXMLReaderInternal::init() {
 	myState = XML_PARSE_DATA;
 }
 
-static const char *ATTRS = { 0 }; 
+static const char *NULL_ATTRS = { 0 }; 
 
 bool ZLXMLReaderInternal::parseBuffer(const char *buffer, size_t len) {
 	const char *start = buffer;
@@ -50,7 +50,33 @@ bool ZLXMLReaderInternal::parseBuffer(const char *buffer, size_t len) {
 										break;
 									}
 								}
-								myReader.startElementHandler(myReader.tag(myTagContents.substr(0, nameLen).c_str()), &ATTRS);
+								const char **attributes = &NULL_ATTRS;
+								char *attributes_string = 0;
+								std::vector<char*> attributes_vector;
+								if (nameLen + 1 < tagLen) {
+									attributes_string = new char[tagLen - nameLen - 1];
+									const char *end = attributes_string + tagLen - nameLen - 1;
+									strncpy(attributes_string, myTagContents.data() + nameLen + 1, tagLen - nameLen - 1);
+									bool read_text = false;
+									for (char *ptr = attributes_string; ptr != end; ptr++) {
+										if (isspace(*ptr) || (*ptr == '"') || (*ptr == '=')) {
+											*ptr = '\0';
+											read_text = false;
+										} else if (!read_text) {
+											attributes_vector.push_back(ptr);
+											read_text = true;
+										}
+									}
+									if (!attributes_vector.empty()) {
+										attributes_vector.push_back(0);
+										attributes = (const char**)&attributes_vector[0];
+									}
+								}
+								myReader.startElementHandler(myReader.tag(myTagContents.substr(0, nameLen).c_str()), attributes);
+								if (attributes_string != 0) {
+									delete[] attributes_string;
+								}
+
 								if (myTagContents[tagLen - 1] == '/') {
 									myReader.endElementHandler(myReader.tag(myTagContents.substr(0, nameLen).c_str()));
 								}
