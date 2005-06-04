84381b08d294cb8de88b2f160aa739888c7a98de
Nikolay Pultsin <geometer@fbreader.org>
2005-06-04 22:24:21 +0000

    optimization (?)
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@267 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- fbreader/desktop-gtk/GtkViewWidget.cpp
+++ fbreader/desktop-gtk/GtkViewWidget.cpp
@@ -58,6 +58,7 @@ GtkViewWidget::~GtkViewWidget() {
 	if (myOriginalPixbuf != 0) {
 		gdk_pixbuf_unref(myOriginalPixbuf);
 		gdk_pixbuf_unref(myRotatedPixbuf);
+		gdk_image_unref(myImage);
 	}
 }
 
@@ -68,17 +69,21 @@ void GtkViewWidget::repaintView()	{
 	gtkContext.updatePixmap(myArea, w, h);
 	view()->paint();
 	if (isRotated()) {
-		ZLTime start0;
 		if ((myOriginalPixbuf != 0) && ((gdk_pixbuf_get_width(myOriginalPixbuf) != w) || (gdk_pixbuf_get_height(myOriginalPixbuf) != h))) {
 			gdk_pixbuf_unref(myOriginalPixbuf);
 			gdk_pixbuf_unref(myRotatedPixbuf);
+			gdk_image_unref(myImage);
 			myOriginalPixbuf = 0;
 		}
 		if (myOriginalPixbuf == 0) {
 			myOriginalPixbuf = gdk_pixbuf_new(GDK_COLORSPACE_RGB, false, 8, w, h);
 			myRotatedPixbuf = gdk_pixbuf_new(GDK_COLORSPACE_RGB, false, 8, h, w);
+			myImage = gdk_image_new(GDK_IMAGE_FASTEST, gdk_drawable_get_visual(gtkContext.pixmap()), w, h);
 		}
-		gdk_pixbuf_get_from_drawable(myOriginalPixbuf, gtkContext.pixmap(), gdk_drawable_get_colormap(gtkContext.pixmap()), 0, 0, 0, 0, w, h);
+		ZLTime start0;
+		gdk_drawable_copy_to_image(gtkContext.pixmap(), myImage, 0, 0, 0, 0, w, h);
+		gdk_pixbuf_get_from_image(myOriginalPixbuf, myImage, gdk_drawable_get_colormap(gtkContext.pixmap()), 0, 0, 0, 0, w, h);
+		//gdk_pixbuf_get_from_drawable(myOriginalPixbuf, gtkContext.pixmap(), gdk_drawable_get_colormap(gtkContext.pixmap()), 0, 0, 0, 0, w, h);
 		ZLTime start1;
 		::rotate(myRotatedPixbuf, myOriginalPixbuf);
 		ZLTime end1;
@@ -90,6 +95,7 @@ void GtkViewWidget::repaintView()	{
 		if (myOriginalPixbuf != 0) {
 			gdk_pixbuf_unref(myOriginalPixbuf);
 			gdk_pixbuf_unref(myRotatedPixbuf);
+			gdk_image_unref(myImage);
 			myOriginalPixbuf = 0;
 		}
 		gdk_draw_pixmap(myArea->window, myArea->style->white_gc, gtkContext.pixmap(), 0, 0, 0, 0, myArea->allocation.width, myArea->allocation.height);
