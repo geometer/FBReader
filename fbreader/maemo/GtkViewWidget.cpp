10f511aeb6e06e8a28f0060ceae98e87c7e9a95e
Mikhail Sobolev <mss@mawhrin.net>
2005-06-06 15:44:11 +0000

    applied the latest stuff for fast rotation
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@281 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- fbreader/maemo/GtkViewWidget.cpp
+++ fbreader/maemo/GtkViewWidget.cpp
@@ -18,14 +18,6 @@
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-// #define ROTATION_TIMING
-
-#ifdef ROTATION_TIMING
-#include <iostream>
-
-#include <abstract/ZLTime.h>
-#endif
-
 #include "GtkViewWidget.h"
 #include "GtkFBReader.h"
 #include "GtkPaintContext.h"
@@ -63,6 +55,7 @@ GtkViewWidget::~GtkViewWidget() {
 	if (myOriginalPixbuf != 0) {
 		gdk_pixbuf_unref(myOriginalPixbuf);
 		gdk_pixbuf_unref(myRotatedPixbuf);
+		gdk_image_unref(myImage);
 	}
 }
 
@@ -76,33 +69,23 @@ void GtkViewWidget::repaintView()	{
 		if ((myOriginalPixbuf != 0) && ((gdk_pixbuf_get_width(myOriginalPixbuf) != w) || (gdk_pixbuf_get_height(myOriginalPixbuf) != h))) {
 			gdk_pixbuf_unref(myOriginalPixbuf);
 			gdk_pixbuf_unref(myRotatedPixbuf);
+			gdk_image_unref(myImage);
 			myOriginalPixbuf = 0;
 		}
 		if (myOriginalPixbuf == 0) {
 			myOriginalPixbuf = gdk_pixbuf_new(GDK_COLORSPACE_RGB, false, 8, w, h);
 			myRotatedPixbuf = gdk_pixbuf_new(GDK_COLORSPACE_RGB, false, 8, h, w);
+			myImage = gdk_image_new(GDK_IMAGE_FASTEST, gdk_drawable_get_visual(gtkContext.pixmap()), w, h);
 		}
-#ifdef ROTATION_TIMING
-    ZLTime c1;
-#endif
-		gdk_pixbuf_get_from_drawable(myOriginalPixbuf, gtkContext.pixmap(), gdk_drawable_get_colormap(gtkContext.pixmap()), 0, 0, 0, 0, w, h);
-#ifdef ROTATION_TIMING
-    ZLTime c2;
-#endif
+		gdk_drawable_copy_to_image(gtkContext.pixmap(), myImage, 0, 0, 0, 0, w, h);
+		gdk_pixbuf_get_from_image(myOriginalPixbuf, myImage, gdk_drawable_get_colormap(gtkContext.pixmap()), 0, 0, 0, 0, w, h);
 		::rotate(myRotatedPixbuf, myOriginalPixbuf);
-#ifdef ROTATION_TIMING
-    ZLTime c3;
-#endif
 		gdk_draw_pixbuf(myArea->window, myArea->style->white_gc, myRotatedPixbuf, 0, 0, 0, 0, h, w, GDK_RGB_DITHER_NONE, 0, 0);
-#ifdef ROTATION_TIMING
-    ZLTime c4;
-
-    std::cout << "Rotate: " << c2.millisecondsFrom(c1) << ", " << c3.millisecondsFrom(c2) << ", " << c4.millisecondsFrom(c3) << ", " << c2.millisecondsFrom(c1)+c3.millisecondsFrom(c2)+c4.millisecondsFrom(c3) << std::endl;
-#endif
 	} else {
 		if (myOriginalPixbuf != 0) {
 			gdk_pixbuf_unref(myOriginalPixbuf);
 			gdk_pixbuf_unref(myRotatedPixbuf);
+			gdk_image_unref(myImage);
 			myOriginalPixbuf = 0;
 		}
 		gdk_draw_pixmap(myArea->window, myArea->style->white_gc, gtkContext.pixmap(), 0, 0, 0, 0, myArea->allocation.width, myArea->allocation.height);
--- fbreader/maemo/gdk-pixbuf-hack.cpp
+++ fbreader/maemo/gdk-pixbuf-hack.cpp
@@ -2,22 +2,21 @@
 
 #include <string.h>
 
-#if 1
 //
 // pixbuf rotation
 // (copied from gqview)
 //
 
 static void pixbuf_copy_block_rotate(guchar *src, gint src_row_stride, gint x, gint y,
-				     guchar *dest, gint dest_row_stride, gint w, gint h,
-				     gint bytes_per_pixel, gint counter_clockwise) {
-  gint i, j;
-  guchar *sp;
-  guchar *dp;
+						 guchar *dest, gint dest_row_stride, gint w, gint h,
+						 gint bytes_per_pixel, gint counter_clockwise) {
+	gint i, j;
+	guchar *sp;
+	guchar *dp;
 
-  for (i = 0; i < h; i++) {
-    sp = src + ((i + y) * src_row_stride) + (x * bytes_per_pixel);
-    for (j = 0; j < w; j++) {
+	for (i = 0; i < h; i++) {
+		sp = src + ((i + y) * src_row_stride) + (x * bytes_per_pixel);
+		for (j = 0; j < w; j++) {
 			if (counter_clockwise) {
 				dp = dest + ((w - j - 1) * dest_row_stride) + (i * bytes_per_pixel);
 			} else {
@@ -28,12 +27,12 @@ static void pixbuf_copy_block_rotate(guchar *src, gint src_row_stride, gint x, g
 			*(dp++) = *(sp++);	/* b */
 			if (bytes_per_pixel == 4) *(dp) = *(sp++);	/* a */
 		}
-  }
+	}
 
 }
 
 static void pixbuf_copy_block(guchar *src, gint src_row_stride, gint w, gint h,
-			      guchar *dest, gint dest_row_stride, gint x, gint y, gint bytes_per_pixel) {
+						guchar *dest, gint dest_row_stride, gint x, gint y, gint bytes_per_pixel) {
 	gint i;
 	guchar *sp;
 	guchar *dp;
@@ -54,74 +53,58 @@ static void pixbuf_copy_block(guchar *src, gint src_row_stride, gint w, gint h,
  */
 // GdkPixbuf *pixbuf_copy_rotate_90(GdkPixbuf *src, gint counter_clockwise)
 void rotate(GdkPixbuf *dest, const GdkPixbuf *src) {
-  gint has_alpha;
-  gint sw, sh, srs;
-  gint drs;
-  guchar *s_pix;
-  guchar *d_pix;
-  gint i, j;
-  gint a;
-  GdkPixbuf *buffer;
-  guchar *b_pix;
-  gint brs;
-  gint w, h;
+	gint has_alpha;
+	gint sw, sh, srs;
+	gint drs;
+	guchar *s_pix;
+	guchar *d_pix;
+	gint i, j;
+	gint a;
+	GdkPixbuf *buffer;
+	guchar *b_pix;
+	gint brs;
+	gint w, h;
 	gint counter_clockwise = true;
 
-  if (src == 0)
+	if (src == 0)
 		return;
 
-  sw = gdk_pixbuf_get_width(src);
-  sh = gdk_pixbuf_get_height(src);
-  has_alpha = gdk_pixbuf_get_has_alpha(src);
-  srs = gdk_pixbuf_get_rowstride(src);
-  s_pix = gdk_pixbuf_get_pixels(src);
+	sw = gdk_pixbuf_get_width(src);
+	sh = gdk_pixbuf_get_height(src);
+	has_alpha = gdk_pixbuf_get_has_alpha(src);
+	srs = gdk_pixbuf_get_rowstride(src);
+	s_pix = gdk_pixbuf_get_pixels(src);
 
-  drs = gdk_pixbuf_get_rowstride(dest);
-  d_pix = gdk_pixbuf_get_pixels(dest);
+	drs = gdk_pixbuf_get_rowstride(dest);
+	d_pix = gdk_pixbuf_get_pixels(dest);
 
-  a = (has_alpha ? 4 : 3);
+	a = (has_alpha ? 4 : 3);
 
-  buffer = gdk_pixbuf_new(GDK_COLORSPACE_RGB, has_alpha, 8, ROTATE_BUFFER_WIDTH, ROTATE_BUFFER_HEIGHT);
-  b_pix = gdk_pixbuf_get_pixels(buffer);
-  brs = gdk_pixbuf_get_rowstride(buffer);
+	buffer = gdk_pixbuf_new(GDK_COLORSPACE_RGB, has_alpha, 8, ROTATE_BUFFER_WIDTH, ROTATE_BUFFER_HEIGHT);
+	b_pix = gdk_pixbuf_get_pixels(buffer);
+	brs = gdk_pixbuf_get_rowstride(buffer);
 
-  for (i = 0; i < sh; i+= ROTATE_BUFFER_WIDTH) {
-    w = MIN(ROTATE_BUFFER_WIDTH, (sh - i));
-    for (j = 0; j < sw; j += ROTATE_BUFFER_HEIGHT) {
-      gint x, y;
+	for (i = 0; i < sh; i+= ROTATE_BUFFER_WIDTH) {
+		w = MIN(ROTATE_BUFFER_WIDTH, (sh - i));
+		for (j = 0; j < sw; j += ROTATE_BUFFER_HEIGHT) {
+			gint x, y;
 
-      h = MIN(ROTATE_BUFFER_HEIGHT, (sw - j));
-      pixbuf_copy_block_rotate(s_pix, srs, j, i, b_pix, brs, h, w, a, counter_clockwise);
+			h = MIN(ROTATE_BUFFER_HEIGHT, (sw - j));
+			pixbuf_copy_block_rotate(s_pix, srs, j, i, b_pix, brs, h, w, a, counter_clockwise);
 
-      if (counter_clockwise) {
-        x = i;
-        y = sw - h - j;
-        } else {
-        x = sh - w - i;
-        y = j;
-      }
+			if (counter_clockwise) {
+				x = i;
+				y = sw - h - j;
+			} else {
+				x = sh - w - i;
+				y = j;
+			}
 
-      pixbuf_copy_block(b_pix, brs, w, h, d_pix, drs, x, y, a);
-    }
-  }
-
-  gdk_pixbuf_unref(buffer);
-}
-
-#else
-// old hack is below
-
-#define OFFSET(pb, x, y) ((x) * (pb)->n_channels + (y) * (pb)->rowstride)
-
-void rotate(GdkPixbuf *dst, const GdkPixbuf *src) {
-	for (gint y = 0; y < src->height; y++) {
-		for (gint x = 0; x < src->width; x++) {
-			const guchar *p = src->pixels + OFFSET (src, x, y);
-			guchar *q = dst->pixels + OFFSET (dst, y, src->width - x - 1);
-			memcpy (q, p, dst->n_channels);
+			pixbuf_copy_block(b_pix, brs, w, h, d_pix, drs, x, y, a);
 		}
 	}
+
+	gdk_pixbuf_unref(buffer);
 }
-#endif
 
 // vim:ts=2:sw=2:noet
