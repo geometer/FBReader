36ee72dc3ca9cb00947a56eaeeac26cf9cc7ca93
Nikolay Pultsin <geometer@fbreader.org>
2005-07-23 13:31:03 +0000

    cleanup
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@416 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- zlibrary/abstract/util/ZLLowMemoryString.h
+++ zlibrary/abstract/util/ZLLowMemoryString.h
@@ -26,65 +26,35 @@ class ZLLowMemoryString {
 public:
 	ZLLowMemoryString() UTIL_SECTION;
 	~ZLLowMemoryString() UTIL_SECTION;
-	//ZLLowMemoryString(const ZLLowMemoryString &s) UTIL_SECTION;
-	//ZLLowMemoryString(const char *s) UTIL_SECTION;
-	//const ZLLowMemoryString &operator = (const ZLLowMemoryString &s) UTIL_SECTION;
-	//const ZLLowMemoryString &operator = (const char *s) UTIL_SECTION;
 	
 	void reserve(size_t size) UTIL_SECTION;
 	void erase() UTIL_SECTION;
-	//void erase(size_t start, size_t len = (size_t)-1) UTIL_SECTION;
-	//const ZLLowMemoryString &operator += (const ZLLowMemoryString &s) UTIL_SECTION;
-	//const ZLLowMemoryString &operator += (char c) UTIL_SECTION;
-	//const ZLLowMemoryString operator + (const ZLLowMemoryString &s) const UTIL_SECTION;
-	//const ZLLowMemoryString operator + (char c) const UTIL_SECTION;
 	void add(size_t offset, const std::string &s) UTIL_SECTION;
+	void operator += (const std::string &s) UTIL_SECTION;
 
 	size_t length() const UTIL_SECTION;
 	bool empty() const UTIL_SECTION;
 	const char *data() const UTIL_SECTION;
-	//const char *c_str() const UTIL_SECTION;
 	char &operator [] (size_t index) const UTIL_SECTION;
-
-	//ZLLowMemoryString substr(size_t start, size_t len = (size_t)-1) const UTIL_SECTION;
-	//void append(const char *s, size_t len) UTIL_SECTION;
-	//void swap(ZLLowMemoryString &s) UTIL_SECTION;
-
-	//int compare(const ZLLowMemoryString &s, int fromPos, int len) const UTIL_SECTION;
-	//bool operator == (const ZLLowMemoryString &s) const UTIL_SECTION;
-	//bool operator != (const ZLLowMemoryString &s) const UTIL_SECTION;
-	//bool operator == (const char *s) const UTIL_SECTION;
-	//bool operator != (const char *s) const UTIL_SECTION;
-	//bool operator < (const ZLLowMemoryString &s) const UTIL_SECTION;
-	//
-	//size_t find(const ZLLowMemoryString &pattern, size_t fromPos = 0) const UTIL_SECTION;
-	//size_t rfind(const ZLLowMemoryString &pattern, size_t fromPos = (size_t)-1) const UTIL_SECTION;
-	//size_t find(char c, size_t fromPos = 0) const UTIL_SECTION;
-	//size_t rfind(char c, size_t fromPos = (size_t)-1) const UTIL_SECTION;
 
 private:
 	size_t myLength;
 	char *myData;
-
-//friend const ZLLowMemoryString operator + (const char *s0, const ZLLowMemoryString &s1) UTIL_SECTION;
 
 private:
 	ZLLowMemoryString(const ZLLowMemoryString &s) UTIL_SECTION;
 	const ZLLowMemoryString &operator = (const ZLLowMemoryString &s) UTIL_SECTION;
 };
 
-//const ZLLowMemoryString operator + (const char *s0, const ZLLowMemoryString &s1) UTIL_SECTION;
-//bool operator == (const char *s0, const ZLLowMemoryString &s1) UTIL_SECTION;
-//bool operator != (const char *s0, const ZLLowMemoryString &s1) UTIL_SECTION;
+inline ZLLowMemoryString::ZLLowMemoryString() : myLength(0), myData(0) {}
+inline ZLLowMemoryString::~ZLLowMemoryString() { if (myData != 0) delete[] myData; }
 
 inline size_t ZLLowMemoryString::length() const { return myLength; }
 inline bool ZLLowMemoryString::empty() const { return myLength == 0; }
 inline const char *ZLLowMemoryString::data() const { return myData; }
 inline char &ZLLowMemoryString::operator [] (size_t index) const { return myData[index]; }
-//inline bool ZLLowMemoryString::operator != (const ZLLowMemoryString &s) const { return !operator == (s); }
-//inline bool ZLLowMemoryString::operator != (const char *s) const { return !operator == (s); }
-//inline bool operator == (const char *s0, const ZLLowMemoryString &s1) { return s1 == s0; }
-//inline bool operator != (const char *s0, const ZLLowMemoryString &s1) { return s1 != s0; }
+
 inline void ZLLowMemoryString::erase() { if (myData != 0) { delete[] myData; myData = 0; } myLength = 0; }
+inline void ZLLowMemoryString::operator += (const std::string &s) { add(myLength, s); }
 
 #endif /* __ZLLOWMEMORYSTRING_H__ */
