5106f96b21cd540ca48c611a8064c8f43ae8d9e3
Nikolay Pultsin <geometer@fbreader.org>
2005-10-03 00:58:28 +0000

    key bindings settings (in progress)
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@710 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- fbreader/common/fbreader/FBReader.h
+++ fbreader/common/fbreader/FBReader.h
@@ -72,6 +72,7 @@ public:
 		ACTION_ROTATE_SCREEN,
 		ACTION_SHOW_LAST_BOOKS,
 		ACTION_QUIT,
+		LAST_ACTION,
 	};
 
 protected:
@@ -117,6 +118,9 @@ public:
 	static ZLStringOption SearchPatternOption;
 
 	static const std::string HelpDirectory;
+
+public:
+	static std::string actionName(ActionCode code);
 
 protected:
 	FBReader(ZLPaintContext *context, const std::string& bookToOpen = std::string()) FB_SECTION;
