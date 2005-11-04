d31c0736d6403858bf61d9282d3c62e520186cd8
Nikolay Pultsin <geometer@fbreader.org>
2005-11-04 21:32:16 +0000

    synchronization (in progress)
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@784 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- build_all.sh
+++ build_all.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 
-TARGETS="desktop-gtk desktop-qt zaurus-qtopia zaurus-pdaxrom-qt zaurus-pdaxrom-gtk qvfb-opie opensimpad-opie"
-
-make clean 1> /dev/null 2>&1;
+do_build() {
+	echo -n "Building $1 ($2) ...";
+	TARGET_ARCH=$1 UI_TYPE=$2 make clean 1> /dev/null 2>&1;
+	if ! TARGET_ARCH=$1 UI_TYPE=$2 make package 1> $1\($2\).log 2>&1; then
+		echo " failure";
+	else
+		echo " OK"
+		rm $1\($2\).log;
+	fi
+	TARGET_ARCH=$1 UI_TYPE=$2 make clean 1> /dev/null 2>&1;
+}
 
 if [ -r makefiles/target.mk ]; then
   mv makefiles/target.mk makefiles/__target.mk
 fi
 
-for target in $TARGETS; do
-	echo -n "Building $target ...";
-	if ! TARGET_ARCH=$target make 1> $target.log 2>&1; then
-		echo " failure";
-		break;
-	fi
-	echo " OK"
-	TARGET_ARCH=$target make clean 1> /dev/null 2>&1;
-	rm $target.log;
-done
+do_build opensimpad-0.9.0 opie
+do_build qvfb opie
+#do_build maemo maemo
+do_build desktop gtk
+do_build desktop qt
+do_build zaurus qtopia
+do_build pdaxrom-1.1.0rc10 gtk
+do_build pdaxrom-1.1.0rc10 qt
+do_build pdaxrom-1.1.0rc11 gtk
+do_build pdaxrom-1.1.0rc11 qt
+#do_build palm palm
 
 if [ -r makefiles/__target.mk ]; then
   mv makefiles/__target.mk makefiles/target.mk
