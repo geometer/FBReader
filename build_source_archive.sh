1741b3fb6b72fde2ac393411ac7e793b5ddbe7ff
Nikolay Pultsin <geometer@fbreader.org>
2005-11-04 21:47:33 +0000

    synchronization
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@786 62a0de91-87f2-0310-aeaa-8eaa5733e38e

---
+++ build_source_archive.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export DIR=fbreader-`cat fbreader/VERSION`
+mkdir $DIR
+cp -r Makefile build_all.sh zlibrary fbreader makefiles $DIR
+
+rm -rf `find $DIR -name ".svn"`
+cd $DIR
+make distclean
+cd ..
+
+tar czf fbreader-sources-`cat fbreader/VERSION`.tgz $DIR
+
+rm -rf $DIR
