6d3637b5010b8623f665433712c688b0b44160ae
Mikhail Sobolev <mss@mawhrin.net>
2005-11-16 21:31:42 +0000

    include CREDITS file in the source archive
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@823 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- build_source_archive.sh
+++ build_source_archive.sh
@@ -2,7 +2,7 @@
 
 export DIR=fbreader-`cat fbreader/VERSION`
 mkdir $DIR
-cp -r Makefile zlibrary fbreader makefiles README.build $DIR
+cp -r Makefile zlibrary fbreader makefiles README.build CREDITS $DIR
 
 rm -rf `find $DIR -name ".svn"`
 cd $DIR
