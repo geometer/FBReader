05fd23c822792c809d4e8b82166971a8f4d0e00f
Nikolay Pultsin <geometer@fbreader.org>
2006-02-22 21:09:24 +0000

    Maemo experiments
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@921 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- zlibrary/maemo/dialogs/GtkWaitMessage.cpp
+++ zlibrary/maemo/dialogs/GtkWaitMessage.cpp
@@ -21,55 +21,21 @@
 #include <gtk/gtkmain.h>
 #include <gtk/gtklabel.h>
 #include <gdk/gdkcursor.h>
+#include <hildon-widgets/gtk-infoprint.h>
 
 #include "GtkWaitMessage.h"
 
 GtkWaitMessage::GtkWaitMessage(GtkWindow *parent, const std::string& message) : myParent(parent) {
-	while (gtk_events_pending()) {
-		gtk_main_iteration();
-	}
-
-	myWindow = GTK_WINDOW(gtk_window_new(GTK_WINDOW_POPUP));
-	gtk_window_set_accept_focus(myWindow, FALSE);
-	GtkWidget *label = gtk_label_new(message.c_str());
-	gtk_misc_set_padding(GTK_MISC(label), 10, 10);    //  something nice?
-	gtk_container_add(GTK_CONTAINER(myWindow), label);
-	gtk_widget_show_all(GTK_WIDGET(myWindow));
-
-	GdkCursor *cursor = gdk_cursor_new(GDK_WATCH);
-
 	if (myParent != 0) {
-		gdk_window_set_cursor(GTK_WIDGET(myParent)->window, cursor);
-	}
-
-	gdk_window_set_cursor(GTK_WIDGET(myWindow)->window, cursor);
-	gdk_cursor_unref(cursor);
-
-	int x, y, w, h;
-	if (myParent != 0) {
-		gtk_window_get_position(myParent, &x, &y);
-		gtk_window_get_size(myParent, &w, &h);
-		x += w / 2;
-		y += h / 2;
-	} else {
-		GdkWindow *root = gdk_screen_get_root_window(gdk_screen_get_default());
-		gdk_window_get_geometry(root, &x, &y, &w, &h, 0);
-		x += w / 2;
-		y += h / 2;
-	}
-	gtk_window_get_size(myWindow, &w, &h);
-	x -= w / 2;
-	y -= h / 2;
-	gtk_window_move(myWindow, x, y);
-
-	while (gtk_events_pending()) {
-		gtk_main_iteration();
+		gtk_banner_show_animation(myParent, message.c_str());
+		while (gtk_events_pending()) {
+			gtk_main_iteration();
+		}
 	}
 }
 
 GtkWaitMessage::~GtkWaitMessage() {
 	if (myParent != 0) {
-		gdk_window_set_cursor(GTK_WIDGET(myParent)->window, 0);
+		gtk_banner_close(myParent);
 	}
-	gtk_widget_destroy(GTK_WIDGET(myWindow));
 }
