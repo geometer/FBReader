696fd258a3389a5fe5e551ae0fc2eca9e6420421
Nikolay Pultsin <geometer@fbreader.org>
2006-05-10 13:50:25 +0000

    refactoring
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@1076 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- fbreader/common/formats/rtf/RtfBookReader.cpp
+++ fbreader/common/formats/rtf/RtfBookReader.cpp
@@ -90,7 +90,6 @@ void RtfBookReader::startDocumentHandler() {
     imageIndex = 0;
     
     footnoteIndex = 1;
-    stack.reserve(3);
     currentStyleInfo = 0;    
     
     state.state = READ_TEXT;
@@ -111,65 +110,47 @@ void RtfBookReader::endDocumentHandler() {
     flushBuffer();
     endParagraph();
     popKind();
-
-    stack.reserve(0);
 }
 
 void RtfBookReader::startElementHandler(int tag) {
-//    DPRINT("start handler: %i\n", tag);
-    switch(tag)
-    {
+  switch(tag) {
     case _P:
-    {
-        flushBuffer();
-        endParagraph();
-        beginParagraph();
-        break;
-    }
+      flushBuffer();
+      endParagraph();
+      beginParagraph();
+      break;
     case _BOLD:
-    {
-        DPRINT("bold start.\n");
-        
-        if (state.state != READ_TEXT)
-        {
+      if (state.state != READ_TEXT) {
         DPRINT("change style not in text.\n");
         break;
-        }
+      }
         
-        flushBuffer();
+      flushBuffer();
         
-        state.isBold = true;
+      state.isBold = true;
         
-        DPRINT("add style strong.\n");
+      DPRINT("add style strong.\n");
         
-        pushKind(STRONG);
-        addControl(STRONG, true);
+      pushKind(STRONG);
+      addControl(STRONG, true);
 
-        break;        
-    }
+      break;        
     case _ITALIC:
-    {
-        DPRINT("italic start.\n");
-        
-        if (state.state != READ_TEXT)
-        {
+      if (state.state != READ_TEXT) {
         DPRINT("change style not in text.\n");
         break;
-        }
+      }
         
-        flushBuffer();
+      flushBuffer();
         
-        state.isItalic = true;
+      state.isItalic = true;
 
-        if (!state.isBold)
-        {        
+      if (!state.isBold) {        
         DPRINT("add style emphasis.\n");
         
         pushKind(EMPHASIS);
         addControl(EMPHASIS, true);
-        }
-        else
-        {
+      } else {
         DPRINT("add style emphasis and strong.\n");
         
         popKind();
@@ -179,98 +160,78 @@ void RtfBookReader::startElementHandler(int tag) {
         addControl(EMPHASIS, true);
         pushKind(STRONG);
         addControl(STRONG, true);
-        }
-        break;
-    }
+      }
+      break;
     case _TITLE_INFO:
     case _AUTHOR:
     case _ENCODING:
-    {
-        state.state = READ_NONE;
-        break;
-    }
+      state.state = READ_NONE;
+      break;
     case _BOOK_TITLE:
-    {
-//        state = READ_CONTENT;
-        state.state = READ_NONE;
-        break;
-    }
+//      state = READ_CONTENT;
+      state.state = READ_NONE;
+      break;
     case _STYLE_INFO:
-    {
-        state.state = READ_NONE;
-
-/*
-        flusBuffer();
-        
-        currentStyleInfo = new StyleInfo;
-        currentStyleInfo.id = atoi(attribute[0]);
-        
-        styles.push_back(currentStyleInfo);
-        
-        state.state = READ_STYLE_INFO;
-*/
-    
-        break;
-    }
+      state.state = READ_NONE;
+      break;
     case _STYLE_SET:
-        state.style = 0;
-        break;
+      state.style = 0;
+      break;
     case _IMAGE:
-        DPRINT("image start.\n");
+      DPRINT("image start.\n");
     
-        flushBuffer();
+      flushBuffer();
 
-        if(!state.isPrevImage)        
-        {
+      if (!state.isPrevImage) {
         endParagraph();
-        }
+      }
 
-        state.isPrevImage = true;
+      state.isPrevImage = true;
         
-        state.state = READ_IMAGE;
+      state.state = READ_IMAGE;
 
-        break;
+      break;
 		case _IMAGE_TYPE:
 			break;
     case _FOOTNOTE:
     {
-        std::string id;
-        std::string id_name;
-        char tmp[256];
+      std::string id;
+      std::string id_name;
+      char tmp[256];
 
-        flushBuffer();
-        sprintf(tmp, "%i", footnoteIndex);
-        id.append(tmp);
+      flushBuffer();
+      sprintf(tmp, "%i", footnoteIndex);
+      id.append(tmp);
 
-        sprintf(tmp, "foonoteIndex%i", footnoteIndex);
-        id_name.append(tmp);
-        
-        DPRINT("footnote reference: %s\n", tmp);
+      sprintf(tmp, "foonoteIndex%i", footnoteIndex);
+      id_name.append(tmp);
+      
+      DPRINT("footnote reference: %s\n", tmp);
 
-        
-        stack.push_back(state);
-        state.id = id_name;
-        state.state = READ_TEXT;
-        state.isItalic = false;
-        state.isBold = false;
-        state.style = -1;
-        state.isPrevImage = false;
-        
-        addHyperlinkControl(FOOTNOTE, id_name);        
-        addData(id);
-        addControl(FOOTNOTE, false);
-        
-        setFootnoteTextModel(id_name);
-        pushKind(REGULAR);
-        beginParagraph();
-        
-        footnoteIndex++;
-        break;
+      
+      stack.push_back(state);
+      state.id = id_name;
+      state.state = READ_TEXT;
+      state.isItalic = false;
+      state.isBold = false;
+      state.style = -1;
+      state.isPrevImage = false;
+      
+      addHyperlinkControl(FOOTNOTE, id_name);        
+      addData(id);
+      addControl(FOOTNOTE, false);
+      
+      setFootnoteTextModel(id_name);
+      pushKind(REGULAR);
+      beginParagraph();
+      
+      footnoteIndex++;
+      break;
     }
     default:
-        state.state = READ_TEXT;
-        break;
-    }
+      state.state = READ_TEXT;
+      break;
+  }
 }
 
 void RtfBookReader::endElementHandler(int tag) {
