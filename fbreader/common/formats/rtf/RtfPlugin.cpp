2f5bf94ac1c4a3f66933a832214b18bf15879703
Nikolay Pultsin <geometer@fbreader.org>
2006-05-14 18:56:19 +0000

    refactoring
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@1123 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- fbreader/common/formats/rtf/RtfPlugin.cpp
+++ fbreader/common/formats/rtf/RtfPlugin.cpp
@@ -33,42 +33,42 @@
 #include "../../description/BookDescription.h"
 
 bool RtfPlugin::acceptsFile(const ZLFile &file) const {
-	const std::string &extension = file.extension();
-	return (extension == "rtf");
+  const std::string &extension = file.extension();
+  return (extension == "rtf");
 }
 
 bool RtfPlugin::readDescription(const std::string &path, BookDescription &description) const {
-	ZLFile file(path);
-	shared_ptr<ZLInputStream> stream = file.inputStream();
+  ZLFile file(path);
+  shared_ptr<ZLInputStream> stream = file.inputStream();
 
-	if (stream.isNull()) {
-		return false;
-	}
+  if (stream.isNull()) {
+    return false;
+  }
 
-	detectEncoding(description, *stream);
+  detectEncoding(description, *stream);
 
-	if (!RtfDescriptionReader(description).readDocument(path)) {
-	  return false;
-	}
-	
-	defaultTitle(description, file.name());
-	defaultLanguage(description);
-	
-	return true;
+  if (!RtfDescriptionReader(description).readDocument(path)) {
+    return false;
+  }
+  
+  defaultTitle(description, file.name());
+  defaultLanguage(description);
+  
+  return true;
 }
 
 bool RtfPlugin::readModel(const BookDescription &description, BookModel &model) const {
-	ZLTime start;
+  ZLTime start;
   int ret = RtfBookReader(model, description.encoding()).readDocument(description.fileName());
-	std::cerr << start.millisecondsTo(ZLTime());
-	return ret;
+  std::cerr << start.millisecondsTo(ZLTime());
+  return ret;
 }
 
 const std::string &RtfPlugin::iconName() const {
-	static const std::string ICON_NAME = "html";
-	return ICON_NAME;
+  static const std::string ICON_NAME = "rtf";
+  return ICON_NAME;
 }
 
 FormatInfoPage *RtfPlugin::createInfoPage(ZLOptionsDialog &dialog, const std::string &fileName) {
-	return new RtfInfoPage(dialog, fileName, "Styles", true);
+  return new RtfInfoPage(dialog, fileName, "Styles", true);
 }
