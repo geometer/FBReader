b077bd5b958339b7ffec85feefc0b6f5ad6f4666
Nikolay Pultsin <geometer@fbreader.org>
2006-06-12 13:38:45 +0000

    added target check in build script for maemo platforms
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@1240 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- build_maemo.sh
+++ build_maemo.sh
@@ -12,9 +12,24 @@ do_build() {
 	make TARGET_ARCH=$1 UI_TYPE=$2 TARGET_STATUS=release clean 1> /dev/null 2>&1;
 }
 
-if [ "$1" == "maemo" -o "$1" == "maemo2" ]; then
-  do_build maemo $1
-else
+if [ "$1" != "maemo" -a "$1" != "maemo2" ]; then
   echo usage:
-	echo "  $0 [maemo | maemo2]"
+  echo "  $0 [maemo | maemo2]"
+  exit 1;
 fi
+
+SDK=`ls /targets/links/scratchbox.config -l | sed -e "s/.\\+\\/\\(.\\+\\)\\.config$/\\1/"`
+
+if [ $1 == "maemo" ]; then
+  EXPECTED_SDK=SDK_ARM
+else
+  EXPECTED_SDK=SDK_ARMEL
+fi
+
+if [ "$SDK" != "$EXPECTED_SDK" ]; then
+  echo Incorrect target: $SDK, $EXPECTED_SDK is expected
+  echo run \'sbox-config -st $EXPECTED_SDK\'
+  exit 1;
+fi
+
+echo do_build maemo $1
