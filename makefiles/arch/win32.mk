09705f668801f0676040faf808289b1116161b39
Nikolay Pultsin <geometer@fbreader.org>
2007-02-25 16:39:30 +0000

    cleanup
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@1640 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- makefiles/arch/win32.mk
+++ makefiles/arch/win32.mk
@@ -11,9 +11,8 @@ AR = i586-mingw32msvc-ar rsu
 LD = i586-mingw32msvc-g++
 RESOURCE_COMPILER = i586-mingw32msvc-windres
 
-CFLAGS = -pipe -fno-exceptions -Wall -Wno-ctor-dtor-privacy -W -I /home/geometer/win32/my/include -DUNICODE -D_WIN32_IE=0x0501 -D_WIN32_WINNT=0x0501
-LDFLAGS = -mwindows -L /home/geometer/win32/my/lib
-#LDFLAGS = -L /home/geometer/win32/my/lib
+CFLAGS = -pipe -fno-exceptions -Wall -Wno-ctor-dtor-privacy -W -DUNICODE -D_WIN32_IE=0x0501 -D_WIN32_WINNT=0x0501
+LDFLAGS = -mwindows
 
 UILIBS = -lgdi32 -lcomctl32
 EXPATLIBS = -lexpat -lz -liconv
