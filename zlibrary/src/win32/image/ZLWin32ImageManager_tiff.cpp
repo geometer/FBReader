3f460be61e6d4a1edabe1f872c62bb0e7d9da0f9
Nikolay Pultsin <geometer@fbreader.org>
2007-03-16 13:39:25 +0000

    win32: added tiff images support
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@1713 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- zlibrary/src/win32/image/ZLWin32ImageManager_tiff.cpp
+++ zlibrary/src/win32/image/ZLWin32ImageManager_tiff.cpp
@@ -21,7 +21,100 @@
 
 #include "ZLWin32ImageManager.h"
 
+class TIFFReader {
+
+public:
+	static tsize_t read(thandle_t readerPtr, tdata_t buffer, tsize_t len);
+	static tsize_t write(thandle_t readerPtr, tdata_t buffer, tsize_t len);
+	static toff_t seek(thandle_t readerPtr, toff_t offset, int);
+	static int close(thandle_t readerPtr);
+	static toff_t size(thandle_t readerPtr);
+	static int map(thandle_t readerPtr, tdata_t*, toff_t*);
+	static void unmap(thandle_t readerPtr, tdata_t, toff_t);
+
+public:
+	TIFFReader(const std::string &data);
+
+private:
+	const std::string &myData;
+	size_t myOffset;
+};
+
+TIFFReader::TIFFReader(const std::string &data) : myData(data), myOffset(0) {
+}
+
+tsize_t TIFFReader::read(thandle_t readerPtr, tdata_t buffer, tsize_t len) {
+	TIFFReader &reader = *(TIFFReader*)readerPtr;
+	len = std::min(len, (tsize_t)(reader.myData.length() - reader.myOffset));
+	if (len > 0) {
+		memcpy(buffer, reader.myData.data() + reader.myOffset, len);
+		reader.myOffset += len;
+	}
+	return len;
+}
+
+tsize_t TIFFReader::write(thandle_t, tdata_t, tsize_t) {
+	// never used
+	return -1;
+}
+
+toff_t TIFFReader::seek(thandle_t readerPtr, toff_t offset, int whence) {
+	TIFFReader &reader = *(TIFFReader*)readerPtr;
+	int newOffset = offset;
+	switch (whence) {
+		case SEEK_SET:
+			break;
+		case SEEK_END:
+			newOffset += reader.myData.length();
+			break;
+		case SEEK_CUR:
+			newOffset += reader.myOffset;
+			break;
+	}
+	if ((newOffset < 0) || (newOffset >= (int)reader.myData.length())) {
+		return (toff_t)-1;
+	}
+	reader.myOffset = newOffset;
+	return reader.myOffset;
+}
+
+int TIFFReader::close(thandle_t) {
+	return 0;
+}
+
+int TIFFReader::map(thandle_t readerPtr, tdata_t *buffer, toff_t *size) {
+	TIFFReader &reader = *(TIFFReader*)readerPtr;
+	*buffer = (tdata_t)reader.myData.data();
+	*size = (toff_t)reader.myData.length();
+	return 1;
+}
+
+void TIFFReader::unmap(thandle_t, tdata_t, toff_t) {
+}
+
+toff_t TIFFReader::size(thandle_t readerPtr) {
+	return ((TIFFReader*)readerPtr)->myData.length();
+}
+
 bool ZLWin32ImageManager::tiffConvert(const std::string &stringData, ZLWin32ImageData &data) const {
-	// TODO: implement
-	return false;
+	TIFFReader reader(stringData);
+	TIFF *tiff = TIFFClientOpen("ZLWin32ImageManager", "rM", &reader, TIFFReader::read, TIFFReader::write, TIFFReader::seek, TIFFReader::close, TIFFReader::size, TIFFReader::map, TIFFReader::unmap);
+	if (tiff == 0) {
+		return false;
+	}
+
+	int width, height;
+	if (!TIFFGetField(tiff, TIFFTAG_IMAGEWIDTH, &width) ||
+			!TIFFGetField(tiff, TIFFTAG_IMAGELENGTH, &height)) {
+		TIFFClose(tiff);
+		return false;
+	}
+
+	data.init(width, height, true, 0);
+
+	TIFFReadRGBAImage(tiff, width, height, (uint32*)data.myArray, 1);
+	data.bgr2rgb();
+
+	TIFFClose(tiff);
+	return true;
 }
