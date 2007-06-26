df81231f3422839dad2eda1356cecd4b4f7f1825
Nikolay Pultsin <geometer@fbreader.org>
2007-06-26 21:38:18 +0000

    fixed linking problem for opensimpad
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@1964 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- makefiles/arch/opensimpad-0.9.0.mk
+++ makefiles/arch/opensimpad-0.9.0.mk
@@ -19,9 +19,9 @@ CC = $(TOOLSPATH)/gcc -I $(OEPATH)/include
 AR = $(TOOLSPATH)/ar rsu
 LD = $(TOOLSPATH)/g++
 
-XML_LIB = -lexpat
-ENCA_LIB = -lenca
-ARCHIVER_LIB = -lbz2
+XML_LIB = -L $(OEPATH)/lib -lexpat
+ENCA_LIB = -L $(OEPATH)/lib -lenca
+ARCHIVER_LIB = -L $(OEPATH)/lib -lbz2
 
 CFLAGS = -pipe -DOPIE_NO_DEBUG -DQT_NO_DEBUG -DQWS -fno-exceptions -fno-rtti -march=armv4 -mtune=xscale -mfloat-abi=soft --param large-function-growth=2000 --param inline-unit-growth=200 -Wall -Wno-ctor-dtor-privacy -W -Winline
 LDFLAGS = -Wl,-rpath,$(LIBDIR)
