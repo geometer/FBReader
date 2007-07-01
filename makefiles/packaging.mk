fad256b268c45bc757d47cca0abcfd6ed4c24045
Nikolay Pultsin <geometer@fbreader.org>
2007-07-01 16:40:17 +0000

    build system refactoring
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@1993 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- makefiles/packaging.mk
+++ makefiles/packaging.mk
@@ -6,5 +6,26 @@
 	@cp -r distributions/$(ARCHITECTURE)-debian $(TMPDIR)/debian
 	@cd $(TMPDIR); dpkg-buildpackage -rfakeroot -us -uc 1> $(CURDIR)/$(ARCHITECTURE)-debian.log 2>&1; cd $(CURDIR)
 	@rm -rf $(TMPDIR)
-	@rm $(CURDIR)/$(ARCHITECTURE)-debian.log
+	@rm -f $(CURDIR)/$(ARCHITECTURE)-debian.log
 	@echo " OK"
+
+.ipk:
+	@echo -en "Building $(ARCHITECTURE) ipk package..."
+	@make -f distributions/$(ARCHITECTURE)-ipk/rules build 1> $(ARCHITECTURE)-ipk.log 2>&1
+	@for controlfile in $(wildcard distributions/$(ARCHITECTURE)-ipk/*.control); do \
+		mkdir $(TMPDIR); \
+		mkdir $(TMPDIR)/data; \
+		make -f distributions/$(ARCHITECTURE)-ipk/rules DESTDIR=$(TMPDIR)/data install-`basename $$controlfile .control` 1>> $(ARCHITECTURE)-ipk.log 2>&1; \
+		sed \
+			-e "s#@VERSION@#`cat zlibrary/VERSION`#" \
+			-e "s#@SIZE@#`du -s -b $(TMPDIR)/data | cut -f1`#" \
+			$$controlfile > $(TMPDIR)/control; \
+		tar czf $(TMPDIR)/control.tar.gz -C $(TMPDIR) ./control; \
+		tar czf $(TMPDIR)/data.tar.gz -C $(TMPDIR)/data .; \
+		echo 2.0 > $(TMPDIR)/debian-binary; \
+		tar czf `egrep ^Filename: $(TMPDIR)/control | cut -d' ' -f2` -C $(TMPDIR) ./debian-binary ./control.tar.gz ./data.tar.gz; \
+		rm -rf $(TMPDIR); \
+	done
+	@echo " OK"
+	@rm -f $(CURDIR)/$(ARCHITECTURE)-ipk.log
+	@make -f distributions/$(ARCHITECTURE)-ipk/rules clean 1> /dev/null 2>&1;
--- tools/build_ipk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-TMP_DIR=$1
-
-tar czf $TMP_DIR/control.tar.gz -C $TMP_DIR ./control
-tar czf $TMP_DIR/data.tar.gz -C $TMP_DIR/data .
-echo 2.0 > $TMP_DIR/debian-binary
-tar czf `egrep ^Filename: $TMP_DIR/control | cut -d' ' -f2` -C $TMP_DIR ./debian-binary ./control.tar.gz ./data.tar.gz
-rm $TMP_DIR/debian-binary
