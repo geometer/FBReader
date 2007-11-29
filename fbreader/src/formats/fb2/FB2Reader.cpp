be2020e39f0cbcea541fc788bec8a904e52c77cc
Nikolay Pultsin <geometer@fbreader.org>
2007-11-29 15:28:15 +0000

    compilation issues
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@2125 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- fbreader/src/formats/fb2/FB2Reader.cpp
+++ fbreader/src/formats/fb2/FB2Reader.cpp
@@ -19,6 +19,8 @@
  * 02110-1301, USA.
  */
 
+#include <string.h>
+
 #include "FB2Reader.h"
 
 void FB2Reader::startElementHandler(const char *t, const char **attributes) {
--- fbreader/src/formats/openreader/ORBookReader.cpp
+++ fbreader/src/formats/openreader/ORBookReader.cpp
@@ -19,6 +19,7 @@
  * 02110-1301, USA.
  */
 
+#include <string.h>
 #include <stdlib.h>
 
 #include <algorithm>
