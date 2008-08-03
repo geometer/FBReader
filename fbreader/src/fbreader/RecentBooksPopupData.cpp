ef8dc1594d77002351faf0e24e6d9ea3d4e4f403
Nikolay Pultsin <geometer@fbreader.org>
2008-08-03 04:36:28 +0000

    synchronization with LCC branch
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@2466 62a0de91-87f2-0310-aeaa-8eaa5733e38e

---
+++ fbreader/src/fbreader/RecentBooksPopupData.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2008 Geometer Plus <contact@geometerplus.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#include "RecentBooksPopupData.h"
+#include "FBReader.h"
+
+RecentBooksPopupData::RecentBooksPopupData(FBReader &fbreader) : myFBReader(fbreader), myId(0) {
+}
+
+void RecentBooksPopupData::updateId() {
+	++myId;
+}
+
+size_t RecentBooksPopupData::id() const {
+	return myId;
+}
+
+size_t RecentBooksPopupData::count() const {
+	return myFBReader.recentBooks().books().size();
+}
+
+const std::string RecentBooksPopupData::text(size_t index) {
+	Books books = myFBReader.recentBooks().books();
+	if (index >= books.size()) {
+		return "";
+	}
+	const BookDescription &book = *books[index];
+	return book.author()->displayName() + ". " + book.title();
+}
+
+void RecentBooksPopupData::run(size_t index) {
+	Books books = myFBReader.recentBooks().books();
+	if (index >= books.size()) {
+		return;
+	}
+	myFBReader.openBook(books[index]);
+	myFBReader.showBookTextView();
+	myFBReader.refreshWindow();
+}
---
+++ fbreader/src/fbreader/RecentBooksPopupData.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2008 Geometer Plus <contact@geometerplus.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#ifndef __RECENTBOOKSPOPUPDATA_H__
+#define __RECENTBOOKSPOPUPDATA_H__
+
+#include <ZLPopupData.h>
+
+class FBReader;
+
+class RecentBooksPopupData : public ZLPopupData {
+
+public:
+	RecentBooksPopupData(FBReader &fbreader);
+	void updateId();
+
+private:
+	size_t id() const;
+	size_t count() const;
+	const std::string text(size_t index);
+	void run(size_t index);
+
+private:
+	FBReader &myFBReader;
+	size_t myId;
+};
+
+#endif /* __RECENTBOOKSPOPUPDATA_H__ */
