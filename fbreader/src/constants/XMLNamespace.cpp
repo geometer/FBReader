aafa16186436d6cf0ea322488feecfd7e0f9a055
Nikolay Pultsin <geometer@fbreader.org>
2008-08-13 14:12:53 +0000

    added xml namespace names table
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@2532 62a0de91-87f2-0310-aeaa-8eaa5733e38e

---
+++ fbreader/src/constants/XMLNamespace.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2008 Geometer Plus <contact@geometerplus.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#include "XMLNamespace.h"
+
+const std::string XMLNamespace::DublinCore = "http://purl.org/dc/elements/1.1/";
+const std::string XMLNamespace::XLink = "http://www.w3.org/1999/xlink";
