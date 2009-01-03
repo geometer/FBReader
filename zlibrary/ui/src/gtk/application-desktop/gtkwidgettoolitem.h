3f8b7dd8d23eb2fca67518b76e26b9233e8dfe56
Nikolay Pultsin <geometer@fbreader.org>
2009-01-03 15:05:46 +0000

    changed year in copyright message
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@2772 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- zlibrary/ui/src/gtk/application-desktop/gtkwidgettoolitem.h
+++ zlibrary/ui/src/gtk/application-desktop/gtkwidgettoolitem.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (C) 2008 Geometer Plus <contact@geometerplus.com>
+ * Copyright (C) 2008-2009 Geometer Plus <contact@geometerplus.com>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
