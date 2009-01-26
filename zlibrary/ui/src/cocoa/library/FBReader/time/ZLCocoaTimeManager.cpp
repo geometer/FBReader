6c207148b9ff060ac5690cc3c78bc97ac528f8df
Alexey Vyskubov <fbreader@fbreader.org>
2009-01-26 05:35:04 +0000

    cosmetic
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@2823 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- zlibrary/ui/src/cocoa/library/FBReader/time/ZLCocoaTimeManager.cpp
+++ zlibrary/ui/src/cocoa/library/FBReader/time/ZLCocoaTimeManager.cpp
@@ -9,9 +9,9 @@
 #include "ZLCocoaTimeManager.h"
 
 void ZLCocoaTimeManager::addTask(shared_ptr<ZLRunnable> task, int interval) {
-	
+	// TO DO
 }
 
 void ZLCocoaTimeManager::removeTask(shared_ptr<ZLRunnable> task) {
-	
+	// TO DO
 }
