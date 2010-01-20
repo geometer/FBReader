09a025f4f6981274c2750ba304e67445ce6e80f5
Nikolay Pultsin <geometer@fbreader.org>
2010-01-20 03:56:45 +0000

    added an iphone 'dummy' code
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@4124 62a0de91-87f2-0310-aeaa-8eaa5733e38e

---
+++ zlibrary/ui/src/iphone/application/ZLCocoaApplicationWindow.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2004-2010 Geometer Plus <contact@geometerplus.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#ifndef __ZLCOCOAAPPLICATIONWINDOW_H__
+#define __ZLCOCOAAPPLICATIONWINDOW_H__
+
+#include "../../../../core/src/desktop/application/ZLDesktopApplicationWindow.h"
+#include "../../../../core/src/application/ZLToolbar.h"
+
+class ZLCocoaViewWidget;
+
+class ZLCocoaApplicationWindow : public ZLDesktopApplicationWindow { 
+
+public:
+	ZLCocoaApplicationWindow(ZLApplication *application);
+	~ZLCocoaApplicationWindow();
+
+public:
+	void restoreSizeAndPosition(void *nsWindow);
+	void saveSizeAndPosition(void *nsWindow);
+	
+private:
+	ZLViewWidget *createViewWidget();
+	void addToolbarItem(ZLToolbar::ItemPtr item);
+	void init();
+	void refresh();
+	void processAllEvents();
+	void close();
+
+	void grabAllKeys(bool grab);
+
+	void setCaption(const std::string &caption);
+
+	void setHyperlinkCursor(bool hyperlink);
+
+	bool isFullscreen() const;
+	void setFullscreen(bool fullscreen);
+
+	void setToggleButtonState(const ZLToolbar::ToggleButtonItem &button);
+	void setToolbarItemState(ZLToolbar::ItemPtr item, bool visible, bool enabled);
+};
+
+#endif /* __ZLCOCOAAPPLICATIONWINDOW_H__ */
---
+++ zlibrary/ui/src/iphone/filesystem/ZLCocoaFSManager.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2004-2010 Geometer Plus <contact@geometerplus.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#ifndef __ZLCOCOAFSMANAGER_H__
+#define __ZLCOCOAFSMANAGER_H__
+
+#include "../../../../core/src/unix/filesystem/ZLUnixFSManager.h"
+
+class ZLCocoaFSManager : public ZLUnixFSManager {
+
+public:
+	static void createInstance() { ourInstance = new ZLCocoaFSManager(); }
+	
+private:
+	ZLCocoaFSManager() {}
+	
+protected:
+	std::string convertFilenameToUtf8(const std::string &name) const;
+};
+
+#endif /* __ZLCOCOAFSMANAGER_H__ */
