5400735040a4b1df014e9f675b146a6aaa80f167
Nikolay Pultsin <geometer@fbreader.org>
2010-04-03 18:36:00 +0000

    more mimetypes have been added
    git-svn-id: https://only.mawhrin.net/repos/stuff/trunk@4501 62a0de91-87f2-0310-aeaa-8eaa5733e38e

--- fbreader/src/constants/MimeType.cpp
+++ fbreader/src/constants/MimeType.cpp
@@ -23,20 +23,32 @@ const std::string MimeType::APPLICATION_XML = "application/xml";
 const std::string MimeType::APPLICATION_ZIP = "application/zip";
 const std::string MimeType::APPLICATION_EPUB_ZIP = "application/epub+zip";
 const std::string MimeType::APPLICATION_FB2_ZIP = "application/fb2+zip";
-const std::string MimeType::APPLICATION_MOBIPOCKET_EBOOK  = "application/x-mobipocket-ebook";
-const std::string MimeType::APPLICATION_PDF   = "application/pdf";
+const std::string MimeType::APPLICATION_MOBIPOCKET_EBOOK = "application/x-mobipocket-ebook";
+const std::string MimeType::APPLICATION_PDF = "application/pdf";
+const std::string MimeType::APPLICATION_CHM = "application/x-chm";
+const std::string MimeType::APPLICATION_PALM_DB = "application/x-palm-database";
 
-const std::string MimeType::APPLICATION_ATOM_XML  = "application/atom+xml";
+const std::string MimeType::APPLICATION_ATOM_XML = "application/atom+xml";
 const std::string MimeType::APPLICATION_ATOM_XML_ENTRY = "application/atom+xml;type=entry";
-const std::string MimeType::APPLICATION_LITRES_XML  = "application/litres+xml";
+const std::string MimeType::APPLICATION_LITRES_XML = "application/litres+xml";
 
-const std::string MimeType::IMAGE_PNG   = "image/png";
-const std::string MimeType::IMAGE_JPEG  = "image/jpeg";
+const std::string MimeType::APPLICATION_GZIP = "application/x-gzip";
+const std::string MimeType::APPLICATION_BZIP2 = "application/x-bzip";
+const std::string MimeType::APPLICATION_TAR = "application/x-tar";
+const std::string MimeType::APPLICATION_TAR_GZIP = "application/x-compressed-tar";
+const std::string MimeType::APPLICATION_TAR_BZIP2 = "application/x-bzip-compressed-tar";
+const std::string MimeType::APPLICATION_TAR_7Z = "application/x-7z-compressed";
+
+const std::string MimeType::IMAGE_PNG = "image/png";
+const std::string MimeType::IMAGE_JPEG = "image/jpeg";
+const std::string MimeType::IMAGE_SVG = "image/svg+xml";
 
 const std::string MimeType::TEXT_HTML = "text/html";
+const std::string MimeType::TEXT_PLAIN = "text/plain";
 
 bool MimeType::isImage(const std::string &mimeType) {
 	return
 		mimeType == IMAGE_PNG ||
-		mimeType == IMAGE_JPEG;
+		mimeType == IMAGE_JPEG ||
+		mimeType == IMAGE_SVG;
 }
--- fbreader/src/constants/MimeType.h
+++ fbreader/src/constants/MimeType.h
@@ -31,14 +31,25 @@ public:
 	static const std::string APPLICATION_FB2_ZIP;
 	static const std::string APPLICATION_MOBIPOCKET_EBOOK;
 	static const std::string APPLICATION_PDF;
+	static const std::string APPLICATION_CHM;
+	static const std::string APPLICATION_PALM_DB;
 	static const std::string APPLICATION_ATOM_XML;
 	static const std::string APPLICATION_ATOM_XML_ENTRY;
 	static const std::string APPLICATION_LITRES_XML;
 
+	static const std::string APPLICATION_GZIP;
+	static const std::string APPLICATION_BZIP2;
+	static const std::string APPLICATION_TAR;
+	static const std::string APPLICATION_TAR_GZIP;
+	static const std::string APPLICATION_TAR_BZIP2;
+	static const std::string APPLICATION_TAR_7Z;
+
 	static const std::string IMAGE_PNG;
 	static const std::string IMAGE_JPEG;
+	static const std::string IMAGE_SVG;
 
 	static const std::string TEXT_HTML;
+	static const std::string TEXT_PLAIN;
 
 public:
 	static bool isImage(const std::string &mimeType);
