cbdaf1c423846488e1380005c1b634108c17be8b
Alexander Turkin <snowwlex@gmail.com>
2011-09-08 14:27:14 +0400

    added network to symbian

--- makefiles/config.mk
+++ makefiles/config.mk
@@ -55,7 +55,7 @@ endif
 ifeq "$(TARGET_ARCH)" "symbian"
   FBREADER_LIBS =  -l:sqlite3.dso 
   EXTERNAL_LIBS = -l:libc.dso -l:libdl.dso -l:euser.dso -l:eexe.lib -l:usrt2_2.lib -l:qtmain.lib -l:dfpaeabi.dso -l:drtaeabi.dso -l:scppnwdl.dso -lsupc++ -lgcc -l:remconcoreapi.dso -l:remconinterfacebase.dso -l:libstdcpp.dso -l:libpthread.dso
-  CORE_LIBS = -l:libm.dso -L$(ROOTDIR)/zlibrary/ui -L$(ROOTDIR)/zlibrary/core -lzlcore -lzlui-$(UI_TYPE) -lzlcore  -l:QtGui.dso -l:QtCore.dso -l:expat.lib -l:ezlib.dso -l:bz2.lib $(FBREADER_LIBS) $(EXTERNAL_LIBS)
+  CORE_LIBS = -l:libm.dso -L$(ROOTDIR)/zlibrary/ui -L$(ROOTDIR)/zlibrary/core -lzlcore -lzlui-$(UI_TYPE) -lzlcore  -l:QtGui.dso -l:QtCore.dso -l:QtNetwork.dso -l:expat.lib -l:ezlib.dso -l:bz2.lib $(FBREADER_LIBS) $(EXTERNAL_LIBS)
   TEXT_LIBS =  -lzltext -l:linebreak.lib  -l:fribidi.lib
 endif
 
