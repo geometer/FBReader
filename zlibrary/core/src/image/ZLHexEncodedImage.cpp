2a7c9e02ab6133099f8118b74a4d582d431a9cea
Alexander Turkin <snowwlex@gmail.com>
2012-06-22 20:19:14 +0400

    RtfPlugin: skipping whitespaces for HexEncodedImage has been added

--- zlibrary/core/src/image/ZLHexEncodedImage.cpp
+++ zlibrary/core/src/image/ZLHexEncodedImage.cpp
@@ -23,37 +23,51 @@
 
 #include "ZLHexEncodedImage.h"
 
-inline static char convertXDigit(char d) {
-	if (isdigit(d)) {
-		return d - '0';
-	} else if (islower(d)) {
-		return d - 'a' + 10;
-	} else {
-		return d - 'A' + 10;
+inline static int decode(char b) {
+	switch (b) {
+		default:
+			return -1;
+		case '0': case '1': case '2': case '3': case '4':
+		case '5': case '6': case '7': case '8': case '9':
+			return b - '0';
+		case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
+			return b - 'A' + 10;
+		case 'a': case 'b': case 'c': case 'd': case 'e': case 'f':
+			return b - 'a' + 10;
 	}
 }
 
 void ZLHexEncodedImage::read() const {
 	shared_ptr<ZLInputStream> stream = ZLFile(myFileName).inputStream();
-	if (!stream.isNull() && stream->open()) {
-		myData = new std::string();
-		myData->reserve(myLength / 2);
-		stream->seek(myStartOffset, false);
-		const size_t bufferSize = 1024;
-		char *buffer = new char[bufferSize];
-		// TODO: skip whitespaces
-		for (unsigned int i = 0; i < myLength; i += bufferSize) {
-			size_t toRead = std::min(bufferSize, myLength - i);
-			if (stream->read(buffer, toRead) != toRead) {
-				break;
-			}
-			for (size_t j = 0; j < toRead; j += 2) {
-				*myData += (convertXDigit(buffer[j]) << 4) + convertXDigit(buffer[j + 1]);
+	if (stream.isNull() || !stream->open()) {
+		return;
+	}
+	myData = new std::string();
+	myData->reserve(myLength / 2);
+	stream->seek(myStartOffset, false);
+	const size_t bufferSize = 1024;
+	char *buffer = new char[bufferSize];
+
+	int first = -1;
+	for (unsigned int i = 0; i < myLength; i += bufferSize) {
+		size_t toRead = std::min(bufferSize, myLength - i);
+		if (stream->read(buffer, toRead) != toRead) {
+			break;
+		}
+		for (size_t j = 0; j < toRead; j += 1) {
+			const int digit = decode(buffer[j]);
+			if (digit != -1) {
+				if (first == -1) {
+					first = digit;
+				} else {
+					*myData += (char)((first << 4) + digit);
+					first = -1;
+				}
 			}
 		}
-		delete[] buffer;
-		stream->close();
 	}
+	delete[] buffer;
+	stream->close();
 }
 
 const shared_ptr<std::string> ZLHexEncodedImage::stringData() const {
