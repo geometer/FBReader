886bd7f0ed284235abfeb5f044e68242763ab67b
Alexander Turkin <snowwlex@gmail.com>
2012-07-03 20:31:53 +0400

    DocPlugin: changed copyrights

--- fbreader/src/formats/doc/OleMainStream.cpp
+++ fbreader/src/formats/doc/OleMainStream.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009-2012 Geometer Plus <contact@geometerplus.com>
+ * Copyright (C) 2004-2012 Geometer Plus <contact@geometerplus.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -966,4 +966,3 @@ unsigned int OleMainStream::getPrlLength(const char *grpprlBuffer, unsigned int 
 			return 1;
 	}
 }
-
