dbbef010842a53dddde10b66198f9542030140a6
Nikolay Pultsin <geometer@fbreader.org>
2012-10-11 12:18:56 +0400

    default alignment => justify

--- fbreader/src/formats/doc/OleMainStream.cpp
+++ fbreader/src/formats/doc/OleMainStream.cpp
@@ -38,7 +38,7 @@ OleMainStream::Style::Style() :
 	LeftIndent(0),
 	FirstLineIndent(0),
 	RightIndent(0),
-	Alignment(ALIGNMENT_LEFT) {
+	Alignment(ALIGNMENT_JUSTIFY) {
 }
 
 OleMainStream::CharInfo::CharInfo() : FontStyle(FONT_REGULAR), FontSize(20) {
