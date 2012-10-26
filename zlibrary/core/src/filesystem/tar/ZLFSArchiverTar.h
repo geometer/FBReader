5d95b3fce79ea0c55b6ccb313dcfeb567b5fc6b0
Nikolay Pultsin <geometer@fbreader.org>
2012-10-26 07:00:19 +0400

    fixed copyright notices

--- zlibrary/core/src/filesystem/tar/ZLFSArchiverTar.h
+++ zlibrary/core/src/filesystem/tar/ZLFSArchiverTar.h
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2004-2012 Geometer Plus <contact@geometerplus.com>
- * Contributed by Serge Osnach <serge.osnach@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
