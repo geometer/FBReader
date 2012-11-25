3a7a361d5fa1026d51eb872c3620fbe62d157912
Nikolay Pultsin <geometer@fbreader.org>
2012-11-25 08:50:53 +0400

    code cleanup

--- zlibrary/core/src/util/ZLStringUtil.cpp
+++ zlibrary/core/src/util/ZLStringUtil.cpp
@@ -47,13 +47,13 @@ bool ZLStringUtil::stringStartsWith(const std::string &str, const std::string &s
 }
 
 bool ZLStringUtil::isDigit(char c) {
-	return (c >= '0') && (c <= '9');
+	return c >= '0' && c <= '9';
 }
 
-bool ZLStringUtil::isLatinLetter(char c){
+bool ZLStringUtil::isLatinLetter(char c) {
 	return
-		((c >= 'A') && (c <= 'Z')) ||
-		((c >= 'a') && (c <= 'z'));
+		(c >= 'A' && c <= 'Z') ||
+		(c >= 'a' && c <= 'z');
 }
 
 
