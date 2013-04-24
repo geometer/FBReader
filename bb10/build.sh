94c1df49701186ab1c3506b233171f6afd818364
Nikolay Pultsin <geometer@fbreader.org>
2013-04-25 00:25:53 +0200

    device pin

--- bb10/build.sh
+++ bb10/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# device pin is 2A1F3971
+
 export PATH=$PATH:/Applications/bbndk/host_10_0_9_404/darwin/x86/usr/bin
 export QMAKESPEC=/Applications/bbndk/target_10_0_9_1673/qnx6/usr/share/qt4/mkspecs/unsupported/blackberry-armv7le-qcc
 export QNX_HOST=/Applications/bbndk/host_10_0_9_404/darwin/x86
